e486c3be7d78183c4f0dbc3393b76f38d377a1f0
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-07-30 20:08:05 -0400

    style: font Outfit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Outfit } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const outfit = Outfit({weight:['400', '700'], subsets: ['latin'] })
 
 export const metadata = {
   title: 'Qr code Frontend Mentor | Esteban Montecinos',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-blue-200`}>{children}</body>
+      <body className={`${outfit.className} bg-blue-200`}>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,17 @@ import imageQr from "../../public/image-qr-code.png";
 export default function Home() {
   return (
     <main className=" flex flex-col gap-2 min-h-screen items-center justify-center p-4">
-      <section className="bg-white flex flex-col gap-4 justify-center items-center min-[385px]:p-6 p-4 rounded-2xl max-w-sm">
+      <section className="bg-white flex flex-col gap-5 justify-center items-center p-4 rounded-2xl max-w-xs">
         <Image className="rounded-xl" src={imageQr} alt="QR Code" />
-        <h1 className="min-[385px]:text-2xl text-xl font-extrabold text-center text-blue-950">
+        <h1 className="text-xl min-[337px]:text-2xl font-bold text-center text-blue-950 leading-tight">
           Improve your front-end skills by building projects
         </h1>
-        <p className="text-gray-500 min-[385px]:text-lg text-sm text-center">
+        <p className="text-gray-500 text-center leading-tight">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
       </section>
-      <footer className="max-w-sm min-[385px]:text-xs text-[0.6rem]"> 
+      <footer className="max-w-sm text-[0.65rem]"> 
     Challenge by <a target="_blank" rel="noopener noreferrer" href="https://www.frontendmentor.io?ref=challenge" className="font-medium underline">Frontend Mentor</a>. 
     Coded by <a target="_blank" rel="noopener noreferrer" href="https://github.com/Esteban-Montecinos" className="font-medium underline">Esteban Montecinos</a>.
   </footer>
